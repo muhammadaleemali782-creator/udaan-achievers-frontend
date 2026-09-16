39934a9842138c319d36a218d1ab7a5cb04f35f8
muhammadaleemali782-creator <muhammadaleemali782@gmail.com>
2026-09-16 20:25:27 +0530

    fix: add self-destruct service worker and aggressive cache-busting headers to force instant browser refresh

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+// Self-destruct script for legacy service worker
+self.addEventListener('install', function() {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() {
+      return self.registration.unregister();
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
